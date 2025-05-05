27d45cf5e4c7c681e55f316f4e20f51ca14a8cbe
aaroncommits <aaroncommits@pm.me>
2025-05-05 14:24:20 -0600

    Add getHumanChoice function and score vars

--- script.js
+++ script.js
@@ -7,3 +7,16 @@ function getComputerChoice(min, max) {
 let computerChoice = getComputerChoice(1,3)
 
 console.log(computerChoice)
+
+function getHumanChoice() {
+  let humanChoice = prompt("What is your choice?") 
+  return humanChoice;
+}
+
+console.log(getHumanChoice())
+
+
+let humanScore = 0
+let computerScore = 0
+
+
