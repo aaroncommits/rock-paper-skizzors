5b4a681287220e0fbf0deb3ac3c8cbde3afbe1bb
aaroncommits <aaroncommits@pm.me>
2025-05-05 17:23:38 -0600

    Complete playGame function and add round logic

--- script.js
+++ script.js
@@ -4,58 +4,89 @@ function getComputerChoiceNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+let computerChoiceNumber 
+
 // Convert to string
 
-function changeComputerChoice(computerChoiceNumber) {
-    if (getComputerChoiceNumber(1,3) === 1) {
-      computerChoiceString = "rock"
-    } else if (getComputerChoiceNumber(1,3) === 2){
-        computerChoiceString = "paper"
-    } else if (getComputerChoiceNumber(1,3) === 3){
-        computerChoiceString = "skizzors"
+function stringComputerChoice(chosenNumber) {
+    if (computerChoiceNumber === 1) {
+      return "rock"
+    } else if (computerChoiceNumber === 2){
+        return "paper"
+    } else if (computerChoiceNumber === 3){
+        return "skizzors"
     }
-    return computerChoiceString;
+    return;
 }
 
-const computerChoice = changeComputerChoice()
+// Get computer choice
 
-console.log(computerChoice)
+function getComputerChoice() {
+  computerChoiceNumber = getComputerChoiceNumber(1,3)
+  computerChoice = stringComputerChoice(computerChoiceNumber)
+  return computerChoice;
+}
+
+let computerChoice 
 
 // Get hooman choice
 
 function getHumanChoice() {
-  let humanChoice = prompt("What is your choice?") 
-  return humanChoice;
+  return prompt("What is your choice?");
 }
 
-const humanChoice = getHumanChoice()
-
-console.log(getHumanChoice())
-
-let humanScore = 0
-let computerScore = 0
-
-// Get score of round
-
-function playRound(humanSelection, computerSelection) {
- 
-  humanChoice.toLowerCase()
+let humanChoice = getHumanChoice()  
   
-  if (computerChoice === humanChoice) {
-    return "It's a tie! No points awarded."
-  } else if (computerChoice == "rock" && humanChoice == "skizzors"){
-    computerScore += 1 
-    return "The computer smashed your skizzors, point for the metal box!"
-  } else if (computerChoice == "skizzors" && humanChoice == "rock"){
-    humanScore += 1
-    return "The hooman has smawhts! Point goes to the hooman for crushing the skizzors."
-  } else if (computerChoice == "paper" && humanChoice == "rock"){
-    computerScore += 1
-    return "The computer covers the pebble with the paper, point goes to the process box."
-  } else if (computerChoice == "rock" && humanChoice == "paper"){
-    humanScore += 1
-    return "The hooman covers the pebble with the paper, point goes to the squishy one."
-  }  
+// Play game specified number of times
+
+function playGame(loops){
+
+  let humanScore = 0
+  let computerScore = 0
+
+  do {
+    
+    getHumanChoice()
+    getComputerChoice()
+    playRound(humanChoice,computerChoice)
+
+    function playRound(humanSelection, computerSelection) {
+   
+      humanChoiceLowered = humanChoice.toLowerCase()
+      
+      if (computerChoice === humanChoiceLowered) {
+        console.log("It's a tie! No points awarded.")
+      } else if (computerChoice == "rock" && humanChoiceLowered == "skizzors"){
+        computerScore += 1 
+        console.log("The computer smashed your skizzors, point for the metal box!");
+      } else if (computerChoice == "skizzors" && humanChoiceLowered == "rock"){
+        humanScore += 1
+        console.log("The hooman has smawhts! Point goes to the hooman for crushing the skizzors.");
+      } else if (computerChoice == "paper" && humanChoiceLowered == "rock"){
+        computerScore += 1
+        console.log("The computer covers the pebble with the paper, point goes to the process box.");
+      } else if (computerChoice == "rock" && humanChoiceLowered == "paper"){
+        humanScore += 1
+        console.log("The hooman covers the pebble with the paper, point goes to the squishy one.");
+      }  
+    }
+    
+    rounds -= 1
+
+  } while (rounds >= 1);
+
+  if (rounds === 0 && humanScore > computerScore) {
+    console.log("Hooman has won the game!");
+  } else if (rounds === 0 && humanScore < computerScore) {
+    console.log("Computing box has won the game!");
+  } else if (rounds === 0 && humanScore == computerScore) {
+    console.log("It's a darn tootin' tie!");
+  }
 }
 
-playRound(humanChoice,computerChoice);
+// Ask how many rounds they want to play
+
+let rounds = parseInt(prompt("How many rounds do you want to play?"))
+
+playGame(rounds)
+
